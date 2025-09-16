cf0b40aebf46b113e17c9f598ad8c86ef3fc160b
Nadzeya Nialiubina <sessiaminsk@gmail.com>
2025-09-16 12:22:17 -0700

    Implement get_friends_favorite_candy_count and create_new_candy_data_structure function

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -1,8 +1,38 @@
+# friend_favorites = [
+#         ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+#         [ "Bob", ["milky way", "licorice", "lollipop"]],
+#         [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+#         [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+#     ]
+
 def get_friends_favorite_candy_count(favorites):
-    pass
+    candy_names = {}
+    
+    for person in favorites:
+        for candy in person[1]:
+            if candy_names[candy] in candy_names:
+                candy_names[candy] += 1
+            else:
+                candy_names[candy] = 1    
+    
+    return candy_names
+
+
 
 def create_new_candy_data_structure(data):
-    pass 
+    specific_candy_dict = {}
+    for person in data:
+        name = person[0]       
+        candies = person[1]
+        for candy in candies:
+            if candy in specific_candy_dict:
+                specific_candy_dict[candy].append(name)
+            else:
+                specific_candy_dict[candy] = [name]
+    
+    return specific_candy_dict              
+
+
 
 def get_friends_who_like_specific_candy(data, candy_name):
     pass
